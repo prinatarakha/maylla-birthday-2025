b9a8d6fc01b77b1b7a4f603b0a05f8cdec2a7f41
rakha <prinatarakha@gmail.com>
2025-06-11 20:50:13 +0700

    task: change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: "Maylla's Birthday 2025",
+  description: "Maylla's Birthday 2025",
 }
 
 export default function RootLayout({
